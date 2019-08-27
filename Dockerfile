cd0567adc88b64a539ecf8160eae9f14dcb51a46
Hadoop <yuhao_hu@live.cn>
2019-08-27 18:39:40 +0800

    Edit config

--- Dockerfile
+++ Dockerfile
@@ -17,10 +17,12 @@ COPY config.json /api/
 EXPOSE 3000
 
 #WORKDIR /api/vendors
+#####首次构建部署时，请将注释打开并对数据库索引和管理员账户进行初始化，管理员账户名在config.json文件中adminAccount配置
 #RUN cd /api/vendors && \
 #    npm run install-server
 
 #WORKDIR ${HOME}
+#### container启动脚本
 COPY docker-entrypoint.sh /api/
 RUN chmod 755 /api/docker-entrypoint.sh
 
--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -3,14 +3,14 @@ set -eo pipefail
 shopt -s nullglob
 
 MY_PORT="${MY_PORT:=3000}"
-MY_ACOUNT="${MY_ACOUNT:=itservice@xiongmaocar.com}"
-MY_DB_SERVER="${MY_DB_SERVER:=dds-2zedd7b1a898bc141.mongodb.rds.aliyuncs.com,dds-2zedd7b1a898bc142.mongodb.rds.aliyuncs.com}"
-MY_DB_NAME="${MY_DB_NAME:=yapi-db-fat}"
-MY_DB_PORT="${MY_DB_PORT:=3717}"
-MY_USER="${MY_USER:=yapi-db-fat}"
-MY_PASS="${MY_PASS:=qVmoDcIXgDybVBlT}"
-MY_AUTH="${MY_AUTH:=yapi-db-fat}"
-replicaSet="${REPLICASET:=mgset-14018575}"
+MY_ACOUNT="${MY_ACOUNT:=****@*****.****}"
+MY_DB_SERVER="${MY_DB_SERVER:=<db_server>}"
+MY_DB_NAME="${MY_DB_NAME:=****}"
+MY_DB_PORT="${MY_DB_PORT:=***}"
+MY_USER="${MY_USER:=***}"
+MY_PASS="${MY_PASS:=****}"
+MY_AUTH="${MY_AUTH:=****}"
+replicaSet="${REPLICASET:=*****}"
 config() {
     if [[ -z "${MY_PORT}" || -z "${MY_ACOUNT}" || -z "${MY_DB_SERVER}" || -z "${MY_DB_NAME}" || -z "${MY_DB_PORT}" || -z "${MY_USER}" || -z "${MY_PASS}" || -z "${MY_AUTH}" || -z ${REPLICASET} ]]; then
         echo -e "\n\"MY_PORT\" or \"MY_ACOUNT\" or \"MY_DB_SERVER\" or \"MY_DB_NAME\" or \"MY_DB_PORT\" or \"MY_USER\" or \"MY_PASS\" or \"MY_AUTH\" can not be empty!\n" && exit 1
